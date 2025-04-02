c5ed5a47e81bace394ee01a13526611acd8d6d45
Alice <habitzreuter21@gmail.com>
2025-04-02 15:05:22 -0300

    debug

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -30,8 +30,10 @@ export default function GameRoom({roomId, userOptions}) {
       setGameData({ gamesPlayed: 0, currentWinner: '-' })
       setGameState('playing');
     } else if(JSON.parse(decodedMsg)['round']) {
+      console.log('received round', JSON.parse(decodedMsg)['round'])
       setCurrentRound([...currentRound, JSON.parse(decodedMsg)['round']])
     } else if(JSON.parse(decodedMsg)['emoji']) {
+      console.log('received emoji', JSON.parse(decodedMsg)['emoji'])
       setCurrentEmoji(JSON.parse(decodedMsg)['emoji'])
     }
   });
